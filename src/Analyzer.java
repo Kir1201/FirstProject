5e307bb124b9e687d03d9b415f3e91435a61cb08
liuxingzhi <xingzhi2@illinois.edu>
2017-12-28 20:50:36 +0800

    "project complete!"

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -2,29 +2,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Analyzer {
 	public static void main(String[] args) {
 		String file = "./src/essay.txt";
 		String essay = readFile(file);
 		String con = "[^a-zA-Z]+";
-//		Pattern p = Pattern.compile(word);
+		// Pattern p = Pattern.compile(word);
 		String test = "abel,kir,pistachio";
 		essay = essay.toLowerCase();
 		String[] fre = essay.split(con);
-		HashMap<String,Integer> map = new HashMap();
+		HashMap<String, Integer> map = new HashMap();
 		for (String a : fre) {
-			if(map.containsKey(a)) {
-				int value = map.get(a);				
+			if (map.containsKey(a)) {
+				int value = map.get(a);
 				map.replace(a, ++value);
-			}else {
+			} else {
 				map.put(a, 1);
 			}
 		}
-		System.out.println(map);
-	}
+//		System.out.println(map);
+		// 这里将map.entrySet()转换成list
+        List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(map.entrySet());
+
+        //然后通过比较器来实现排序
+
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>() {
+            //升序排序
+            public int compare(Entry<String, Integer> o1,
+                    Entry<String, Integer> o2) {
+                return -o1.getValue().compareTo(o2.getValue());
+            }
+        });
+
+        for(Entry<String, Integer> mapping:list){ 
+               System.out.println(mapping.getKey()+":"+mapping.getValue()); 
+          } 
+    }
+
+	
 
 	public static String readFile(String filename) {
 		String s = null;
@@ -55,6 +73,5 @@ public class Analyzer {
 	// }
 	// return maze;
 	// }
-	
-	
+
 }
